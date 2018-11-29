c91819528653111254b258fe354b0891716b7573
Hansel Tertius <hansel.tertius@my.jcu.edu.au>
2018-11-29 14:29:21 +0700

    Create layout for main activity

--- app/src/main/java/com/example/android/inventory/MainActivity.java
+++ app/src/main/java/com/example/android/inventory/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.inventory;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Do nothing for now
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_main.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on the menu option in the app bar overflow menu.
+        switch (item.getItemId()){
+            // Respond when clicked "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+                return true;
+            // Respond when clicked "Delete all inventories" menu option
+            case R.id.action_delete_all_entries:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

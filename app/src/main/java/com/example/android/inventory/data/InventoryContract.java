b60d8f5ea7fbcecf0a531ece8d4aeb6a85cedd55
Hansel Tertius <hansel.tertius@my.jcu.edu.au>
2018-11-30 11:10:10 +0700

    Add InventoryContract class

---
+++ app/src/main/java/com/example/android/inventory/data/InventoryContract.java
@@ -0,0 +1,29 @@
+package com.example.android.inventory.data;
+
+import android.provider.BaseColumns;
+
+public class InventoryContract {
+
+    private InventoryContract(){}
+
+    public static final class InventoryEntry implements BaseColumns {
+
+        /** Name of database table for inventories */
+        public final static String TABLE_NAME = "inventories";
+
+        public final static String _ID = BaseColumns._ID;
+
+        public final static String COLUMN_INVENTORY_BRAND = "brand";
+
+        public final static String COLUMN_INVENTORY_NAME = "name";
+
+        public final static String COLUMN_INVENTORY_PRICE = "price";
+
+        public final static String COLUMN_INVENTORY_QUANTITY = "quantity";
+
+        public final static String COLUMN_INVENTORY_PHONE_NUMBER = "phone_number";
+
+        public final static String COLUMN_INVENTORY_PHONE_EMAIL = "email";
+
+    }
+}

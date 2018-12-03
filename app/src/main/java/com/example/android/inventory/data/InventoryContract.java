6915d9e37dbcc3b3a450a43469915d2801177994
Hansel Tertius <hansel.tertius@my.jcu.edu.au>
2018-12-03 14:09:25 +0700

    Add URIMatcher to ContentProvider

--- app/src/main/java/com/example/android/inventory/data/InventoryContract.java
+++ app/src/main/java/com/example/android/inventory/data/InventoryContract.java
@@ -1,12 +1,26 @@
 package com.example.android.inventory.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class InventoryContract {
 
     private InventoryContract(){}
 
+    /** Initialize content authority, which represent the package of an app */
+    public static final String CONTENT_AUTHORITY = "com.example.android.inventory";
+
+    /** Initialize Uri object by calling the method parse, which takes string and return Uri */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /** Initiate PATH_TableName to be appended to the base content URI */
+    public static final String PATH_INVENTORIES = "inventories";
+
     public static final class InventoryEntry implements BaseColumns {
+
+        /** Initiate complete content uri with withAppendedPath method from Uri, which is to
+         * access the data in the provider */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_INVENTORIES);
 
         /** Name of database table for inventories */
         public final static String TABLE_NAME = "inventories";
--- app/src/main/java/com/example/android/inventory/data/InventoryProvider.java
+++ app/src/main/java/com/example/android/inventory/data/InventoryProvider.java
@@ -2,6 +2,7 @@ package com.example.android.inventory.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -10,8 +11,31 @@ public class InventoryProvider extends ContentProvider {
     /** Tag for the log messages */
     public static final String LOG_TAG = InventoryDbHelper.class.getSimpleName();
 
-    /** Database Helper object*/
+    /** Database Helper object */
     private InventoryDbHelper inventoryDbHelper;
+
+    /** URI matcher code for the content URI for the inventories table */
+    private static final int INVENTORIES = 100;
+
+    /** URI matcher code for the content URI for a single pet in the inventories table */
+    private static final int INVENTORY_ID = 101;
+
+    /**
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     * It's common to use NO_MATCH as the input for this case.
+     */
+    private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        // The calls to addURI() go here, for all of the content URI patterns that the provider
+        // should recognize. All paths added to the UriMatcher have a corresponding code to return
+        // when a match is found.
+
+        uriMatcher.addURI(InventoryContract.CONTENT_AUTHORITY, InventoryContract.PATH_INVENTORIES, INVENTORIES);
+        uriMatcher.addURI(InventoryContract.CONTENT_AUTHORITY, InventoryContract.PATH_INVENTORIES + "/#", INVENTORY_ID);
+    }
 
     /**
      * Initialize the provider and the database helper object.
